1fe163faaa45bd295fadfd376aff3158fabf1a79
Yab1 <yabilisanu@gmail.com>
2023-04-11 19:36:35 +0300

    added a material icon

--- components/Todo.js
+++ components/Todo.js
@@ -1,29 +1,32 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-  TouchableOpacity,
-} from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+<MaterialIcons name="delete" size={24} color="black" />;
 
 export default function Todo({ todo, deleteTodos }) {
   return (
     <TouchableOpacity onPress={() => deleteTodos(todo.item.key)}>
-      <Text style={styles.btnTxt}>{todo.item.title}</Text>
+      <View style={styles.todo}>
+        <MaterialIcons name="delete" size={18} color="#333" />
+        <Text style={styles.todoText}>{todo.item.title}</Text>
+      </View>
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
-  btnTxt: {
-    color: 'black',
+  todo: {
     padding: 16,
     margin: 10,
-    fontSize: 20,
     borderColor: '#bbb',
     borderRadius: 10,
     borderStyle: 'dashed',
     borderWidth: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  todoText: {
+    fontSize: 20,
+    marginLeft: 10,
   },
 });

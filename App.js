4cb12bd80522f688d2095830228067613659e70a
Yab1 <yabilisanu@gmail.com>
2023-04-11 17:15:36 +0300

    phase 1

---
+++ App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, FlatList } from 'react-native';
+import Header from './components/Header';
+import Todo from './components/Todo';
+import TodoForm from './components/TodoForm';
+
+export default function App() {
+  const [value, setValue] = useState('');
+  const [todos, setTodos] = useState([
+    { title: 'take out the trush', key: '1' },
+    { title: 'creat a simple todo app', key: '2' },
+    { title: 'initalize the project', key: '3' },
+  ]);
+
+  const inputHandler = (text) => {
+    setValue(text);
+  };
+
+  const deleteTodos = (key) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.key !== key));
+  };
+
+  const addTodo = () => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { title: value, key: Math.random().toString() },
+    ]);
+    setValue('');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Header />
+      <TodoForm value={value} inputHandler={inputHandler} addTodo={addTodo} />
+      <FlatList
+        data={todos}
+        renderItem={(item) => <Todo todo={item} deleteTodos={deleteTodos} />}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
